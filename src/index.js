ea71bcbb7c45d644a45f0aaf4aa8b6037bffb206
Anton Karpenko <antykkarp@gmail.com>
2019-06-11 22:56:00 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as serviceWorker from "./serviceWorker";
+
+import App from "./components/App";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
