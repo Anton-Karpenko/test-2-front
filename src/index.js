e7e5fe79c754b95d02339f49ea6be277a29525f7
Anton Karpenko <antykkarp@gmail.com>
2019-06-11 23:02:56 +0300

    Set up redux - Create empty rootReducer for storage init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import * as serviceWorker from "./serviceWorker";
 
+import { applyMiddleware, createStore } from "redux";
+
 import App from "./components/App";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
+import ReduxPromise from "redux-promise";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
